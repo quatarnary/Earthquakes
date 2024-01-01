fb8e7ece37c41cfe0610e12c937b1558a9e1bd8c
quatarnary <contact@ilyasbugra.com>
2024-01-01 22:43:22 +0300

    Initial Commit

---
+++ Earthquakes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Earthquakes
+//
+//  Created by Bugra Aslan on 1.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Bugra Aslan on 1.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
