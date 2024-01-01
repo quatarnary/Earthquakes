b6153f46655dc383ef50e8f6d8e0ae4dda8d7bd6
quatarnary <contact@ilyasbugra.com>
2024-01-01 23:17:03 +0300

    model codes implemented

--- Earthquakes/Models/Quake+Color.swift
+++ Earthquakes/Models/Quake+Color.swift
@@ -5,4 +5,23 @@
 //  Created by Bugra Aslan on 1.01.2024.
 //
 
-import Foundation
+import SwiftUI
+
+extension Quake {
+    var color: Color {
+        switch magnitude {
+        case 0..<1:
+            return .green
+        case 1..<2:
+            return .yellow
+        case 2..<3:
+            return .orange
+        case 3..<5:
+            return .red
+        case 5..<Double.greatestFiniteMagnitude:
+            return .init(red: 0.8, green: 0.2, blue: 0.7)
+        default:
+            return .gray
+        }
+    }
+}
--- Earthquakes/Models/Quake.swift
+++ Earthquakes/Models/Quake.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct Quake {
+    let magnitude: Double
+    let place: String
+    let time: Date
+    let code: String
+    let detail: URL
+}
+
+extension Quake: Identifiable {
+    var id: String { code }
+}
