c159f661740c493ede63ee506d5cb691d8615c64
quatarnary <contact@ilyasbugra.com>
2024-01-02 21:16:07 +0300

    QuakeRow implemented

--- Earthquakes/Views/QuakeRow.swift
+++ Earthquakes/Views/QuakeRow.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct QuakeRow: View {
+    var quake: Quake
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            QuakeMagnitude(quake: quake)
+            VStack (alignment: .leading) {
+                Text(quake.place)
+                    .font(.title3)
+                Text("\(quake.time.formatted(.relative(presentation: .named)))")
+                    .foregroundStyle(.secondary)
+            }
+        }
+        .padding(.vertical, 8)
     }
 }
 
 #Preview {
-    QuakeRow()
+    let previewQuake = Quake(magnitude: 1.0,
+                             place: "Shakey Acres",
+                             time: Date(timeIntervalSinceNow: -1000),
+                             code: "nc73649170",
+                             detail: URL(string: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/nc73649170.geojson")!)
+    
+    return QuakeRow(quake: previewQuake)
+        .previewLayout(.sizeThatFits)
 }
