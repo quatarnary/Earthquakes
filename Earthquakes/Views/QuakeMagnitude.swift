77fa8d3b0bb6d2b8f4d754c516fe6a5dab536a93
quatarnary <contact@ilyasbugra.com>
2024-01-02 21:32:26 +0300

    QuakeMagnitude implemented, starting project cathed up, everything seems to be working

--- Earthquakes/Views/QuakeMagnitude.swift
+++ Earthquakes/Views/QuakeMagnitude.swift
@@ -8,11 +8,27 @@
 import SwiftUI
 
 struct QuakeMagnitude: View {
+    var quake: Quake
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        RoundedRectangle(cornerRadius: 8)
+            .fill(.black)
+            .frame(width: 80, height: 60)
+            .overlay {
+                Text("\(quake.magnitude.formatted(.number.precision(.fractionLength(1))))")
+                    .font(.title)
+                    .bold()
+                    .foregroundStyle(quake.color)
+            }
     }
 }
 
 #Preview {
-    QuakeMagnitude()
+    let previewQuake = Quake(magnitude: 1.0,
+                         place: "Shakey Acres",
+                         time: Date(timeIntervalSinceNow: -1000),
+                         code: "nc73649170",
+                         detail: URL(string: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/nc73649170.geojson")!)
+    
+    return QuakeMagnitude(quake: previewQuake)
 }
