e7ea3279a92e059ac90d9c8b684755f4c2f1dd1c
quatarnary <contact@ilyasbugra.com>
2024-01-02 20:56:16 +0300

    ToolbarStatus implemented

--- Earthquakes/Views/ToolbarContent/ToolbarStatus.swift
+++ Earthquakes/Views/ToolbarContent/ToolbarStatus.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct ToolbarStatus: View {
+    var isLoading: Bool
+    var lastUpdated: TimeInterval
+    var quakesCount: Int
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if isLoading {
+                Text("Checking for Earthquakes...")
+                Spacer()
+            } else if lastUpdated == Date.distantFuture.timeIntervalSince1970 {
+                Spacer()
+                Text("\(quakesCount) Earthquakes")
+                    .foregroundStyle(Color.secondary)
+            } else {
+                let lastUpdatedDate = Date(timeIntervalSince1970: lastUpdated)
+                Text("Updated \(lastUpdatedDate.formatted(.relative(presentation: .named)))")
+                Text("\(quakesCount) Earthquakes")
+                    .foregroundStyle(Color.secondary)
+            }
+        }
+        .font(.caption)
     }
 }
 
 #Preview {
-    ToolbarStatus()
+    Group {
+        ToolbarStatus(isLoading: true, lastUpdated: Date.distantPast.timeIntervalSince1970, quakesCount: 125)
+            .previewLayout(.fixed(width: 200, height: 40))
+        ToolbarStatus(isLoading: false, lastUpdated: Date.distantFuture.timeIntervalSince1970, quakesCount: 10_000)
+            .previewLayout(.fixed(width: 200, height: 40))
+        ToolbarStatus(isLoading: false, lastUpdated: Date.now.timeIntervalSince1970, quakesCount: 10_000)
+            .previewLayout(.fixed(width: 200, height: 40))
+        ToolbarStatus(isLoading: false, lastUpdated: Date.distantPast.timeIntervalSince1970, quakesCount: 10_000)
+            .previewLayout(.fixed(width: 200, height: 40))
+    }
 }
