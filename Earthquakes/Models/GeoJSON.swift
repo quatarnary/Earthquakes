3374e7cfef3c680f9f7c1b140d007f1a924bead0
quatarnary <contact@ilyasbugra.com>
2024-01-02 23:30:25 +0300

    section 3 completed

---
+++ Earthquakes/Models/GeoJSON.swift
@@ -0,0 +1,33 @@
+//
+//  GeoJSON.swift
+//  Earthquakes
+//
+//  Created by Bugra Aslan on 2.01.2024.
+//
+
+import Foundation
+
+struct GeoJSON: Decodable {
+    
+    private enum RootCodingKeys: String, CodingKey {
+        case features
+    }
+    private enum FeatureCodingKeys: String, CodingKey {
+        case properties
+    }
+    
+    private(set) var quakes: [Quake] = []
+    
+    init(from decoder: Decoder) throws {
+        let rootContainer = try decoder.container(keyedBy: RootCodingKeys.self)
+        var featuresContainer = try rootContainer.nestedUnkeyedContainer(forKey: .features)
+        
+        while !featuresContainer.isAtEnd {
+            let propertiesContainer = try featuresContainer.nestedContainer(keyedBy: FeatureCodingKeys.self)
+            
+            if let properties = try? propertiesContainer.decode(Quake.self, forKey: .properties) {
+                quakes.append(properties)
+            }
+        }
+    }
+}
