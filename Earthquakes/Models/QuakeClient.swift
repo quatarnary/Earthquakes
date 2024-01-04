5e4ce20b958a10ad3440c030f7c5857c28bd0eb7
quatarnary <contact@ilyasbugra.com>
2024-01-04 23:28:53 +0300

    tutorial break: section 1 step 6 complete

---
+++ Earthquakes/Models/QuakeClient.swift
@@ -0,0 +1,25 @@
+//
+//  QuakeClient.swift
+//  Earthquakes
+//
+//  Created by Bugra Aslan on 4.01.2024.
+//
+
+import Foundation
+
+class QuakeClient {
+    
+    private lazy var decoder: JSONDecoder = {
+        let aDecoder = JSONDecoder()
+        aDecoder.dateDecodingStrategy = .millisecondsSince1970
+        return aDecoder
+    }()
+    
+    private let feedURL = URL(string: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson")!
+    
+    private let downloader: any HTTPDataDownloader
+    
+    init(downloader: any HTTPDataDownloader = URLSession.shared) {
+        self.downloader = downloader
+    }
+}
