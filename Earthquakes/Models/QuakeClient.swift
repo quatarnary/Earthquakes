69cea31e50003e7017ac597215e823aa375729a6
quatarnary <contact@ilyasbugra.com>
2024-01-05 16:21:18 +0300

    section 1 completed

--- Earthquakes/Models/QuakeClient.swift
+++ Earthquakes/Models/QuakeClient.swift
@@ -9,6 +9,14 @@ import Foundation
 
 class QuakeClient {
     
+    var quakes: [Quake] {
+        get async throws {
+            let data = try await downloader.httpData(from: feedURL)
+            let allQuakes = try decoder.decode(GeoJSON.self, from: data)
+            return allQuakes.quakes
+        }
+    }
+    
     private lazy var decoder: JSONDecoder = {
         let aDecoder = JSONDecoder()
         aDecoder.dateDecodingStrategy = .millisecondsSince1970
