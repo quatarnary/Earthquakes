6d961c692960cd1082cd64c34a2db445032a16dd
quatarnary <contact@ilyasbugra.com>
2024-01-06 14:39:36 +0300

    added unexpectedError to QuakeErrors. it was in the starting project but not in tutorial :3

--- Earthquakes/Models/QuakeError.swift
+++ Earthquakes/Models/QuakeError.swift
@@ -10,6 +10,7 @@ import Foundation
 enum QuakeError: Error {
     case missingData
     case networkError
+    case unexpectedError(error: Error)
 }
 
 extension QuakeError: LocalizedError {
@@ -22,6 +23,10 @@ extension QuakeError: LocalizedError {
             
         case .networkError:
             return NSLocalizedString("Error fetching quake data over the network.", comment: "")
+            
+        case .unexpectedError(let error):
+            return NSLocalizedString("Received unexpected error. \(error.localizedDescription)", comment: "")
+
         }
     }
 }
---
+++ Earthquakes/Models/QuakesProvider.swift
@@ -0,0 +1,8 @@
+//
+//  QuakesProvider.swift
+//  Earthquakes
+//
+//  Created by Bugra Aslan on 6.01.2024.
+//
+
+import Foundation
