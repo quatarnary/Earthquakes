f14319766d1cee0203a99115411eb4430aab3b59
quatarnary <contact@ilyasbugra.com>
2024-01-12 22:58:05 +0300

    section 1 experiment halted

--- Earthquakes/Views/QuakeDetail.swift
+++ Earthquakes/Views/QuakeDetail.swift
@@ -19,6 +19,19 @@ struct QuakeDetail: View {
             Text("\(quake.time.formatted(date: Date.FormatStyle.DateStyle.long, time: Date.FormatStyle.TimeStyle.shortened))")
                 .foregroundStyle(Color.secondary)
             if let location = quake.location {
+                // TODO: Trying the experiment part -failed for now
+//                var latitude = location.latitude.formatted(.number.precision(.fractionLength(3)))
+//                var longitude = location.longitude.formatted(.number.precision(.fractionLength(3)))
+//
+//                var latitudeShort = latitude
+//                var longitudeShort = longitude
+//                
+//                var latitudePrecision = location.latitude
+//                var longitudePrecision = location.longitude
+//                Text("Latitude: \(latitude)")
+//                    .onTapGesture {
+//                    }
+                
                 Text("Latitude: \(location.latitude.formatted(.number.precision(.fractionLength(3))))")
                 Text("Longitude: \(location.longitude.formatted(.number.precision(.fractionLength(3))))")
             }
