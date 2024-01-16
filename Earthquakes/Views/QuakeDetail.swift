842e442c25fea2b58aa486499fad134fde9d65f9
quatarnary <contact@ilyasbugra.com>
2024-01-16 20:35:45 +0300

    section 2 completed

--- Earthquakes/Views/QuakeDetail.swift
+++ Earthquakes/Views/QuakeDetail.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct QuakeDetail: View {
     var quake: Quake
+    @EnvironmentObject private var quakesProvider: QuakesProvider
+    @State private var location: QuakeLocation? = nil
     
     var body: some View {
         VStack {
@@ -18,22 +20,18 @@ struct QuakeDetail: View {
                 .bold()
             Text("\(quake.time.formatted(date: Date.FormatStyle.DateStyle.long, time: Date.FormatStyle.TimeStyle.shortened))")
                 .foregroundStyle(Color.secondary)
-            if let location = quake.location {
-                // TODO: Trying the experiment part -failed for now
-//                var latitude = location.latitude.formatted(.number.precision(.fractionLength(3)))
-//                var longitude = location.longitude.formatted(.number.precision(.fractionLength(3)))
-//
-//                var latitudeShort = latitude
-//                var longitudeShort = longitude
-//                
-//                var latitudePrecision = location.latitude
-//                var longitudePrecision = location.longitude
-//                Text("Latitude: \(latitude)")
-//                    .onTapGesture {
-//                    }
-                
+            if let location = self.location {
                 Text("Latitude: \(location.latitude.formatted(.number.precision(.fractionLength(3))))")
                 Text("Longitude: \(location.longitude.formatted(.number.precision(.fractionLength(3))))")
+            }
+        }
+        .task {
+            if self.location  == nil {
+                if let quakeLocation = quake.location {
+                    self.location = quake.location
+                } else {
+                    self.location = try? await quakesProvider.location(for: quake)
+                }
             }
         }
     }
@@ -42,3 +40,22 @@ struct QuakeDetail: View {
 #Preview {
     QuakeDetail(quake: Quake.preview)
 }
+
+
+//    // TODO: Trying the experiment part -failed for now
+//if let location = quake.location {
+////                var latitude = location.latitude.formatted(.number.precision(.fractionLength(3)))
+////                var longitude = location.longitude.formatted(.number.precision(.fractionLength(3)))
+////
+////                var latitudeShort = latitude
+////                var longitudeShort = longitude
+////
+////                var latitudePrecision = location.latitude
+////                var longitudePrecision = location.longitude
+////                Text("Latitude: \(latitude)")
+////                    .onTapGesture {
+////                    }
+//    
+//    Text("Latitude: \(location.latitude.formatted(.number.precision(.fractionLength(3))))")
+//    Text("Longitude: \(location.longitude.formatted(.number.precision(.fractionLength(3))))")
+//}
